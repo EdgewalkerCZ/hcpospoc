615effd9d218d268ac9d02cbd0398b3252610e7a
EdgewalkerCZ <josef.vecernik@juicymo.cz>
2018-08-27 05:05:36 +0530

    next iteration

--- be/src/hcpospoc_be/hcpospoc/views.py
+++ be/src/hcpospoc_be/hcpospoc/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HelloApiView(APIView):
+    """Test API view"""
+
+    def get(self, request, format=None):
+        """Return list of APIView features"""
+
+        an_apiview = [
+            'Uses HTTP Methods as a function (get, post, patch, put, delete)',
+            'Similar to traditional django',
+            'isMappedManuallytoURL',
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
