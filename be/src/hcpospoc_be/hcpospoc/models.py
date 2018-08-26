e77ee642373e3d23a0ca981bc81c1b16173d41cc
WINDOWS-VIJUBLD\VirtualReality <josef.vecernik@juicymo.cz>
2018-08-27 03:51:43 +0530

    userprofilemanager

--- be/src/hcpospoc_be/hcpospoc/models.py
+++ be/src/hcpospoc_be/hcpospoc/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Helps Django to work with custom user model"""
+
+    def create_user(self, email, name, password=None):
+
+        if not email:
+            raise ValueError('Users must have an email adress.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """create and saves a new superuser"""
+
+        user = self.create_user(email,name,password)
+
+        user.isSuperuser = True
+        user.isStaff = True
+
+        user.save(using=self._db)
+
+        return user
+
+class pos_profile(AbstractBaseUser, PermissionsMixin):
+    """Represent POS profile"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    isActive = models.BooleanField(default=True)
+    isStaff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Get Full Usernamen"""
+
+        return self.NAME
+
+    def get_short_name(self):
+        """Used to get users short name"""
+
+        return self.name
+
+    def __str__(self):
+        """Django Uses this when it needs to convert object to string"""
+
+        return self.email
