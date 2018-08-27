71d0ec0eea5e0d2adbbc41db106e55c0ff0e3b20
EdgewalkerCZ <josef.vecernik@juicymo.cz>
2018-08-27 09:00:54 +0530

    next iter

---
+++ be/src/hcpospoc_be/hcpospoc/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+
+class HelloSerializer(serializers.Serializer):
+    """Name field serialize for testing"""
+
+    name = serializers.CharField(max_length=10)
+
+    
--- be/src/hcpospoc_be/hcpospoc/views.py
+++ be/src/hcpospoc_be/hcpospoc/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+
+from . import serializers
 
 # Create your views here.
 
 class HelloApiView(APIView):
     """Test API view"""
+
+    serializer_class = serializers.HelloSerializer
+
 
     def get(self, request, format=None):
         """Return list of APIView features"""
@@ -17,3 +24,16 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self,request):
+        """Create hello message with name"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
