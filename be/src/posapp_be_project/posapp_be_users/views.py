715901257f504eaf554d9005825fa53e50a2eb72
EdgewalkerCZ <josef.vecernik@juicymo.cz>
2018-08-29 06:29:51 +0530

    viewset test

--- be/src/posapp_be_project/posapp_be_users/views.py
+++ be/src/posapp_be_project/posapp_be_users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -55,3 +56,16 @@ class HelloApiView(APIView):
         """Patch request, only updates fields required"""
 
         return Response({'method': 'delete'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API Viewset"""
+
+    def list(self, request):
+        """Returns hello message"""
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
