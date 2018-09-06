4b76ebf471571ad8ca7d3ad7c27b3496e47a04fc
EdgewalkerCZ <vecernikjosef@gmail.com>
2018-09-06 14:57:21 +0530

    Django version

--- be/src/posapp_be_project/posapp_be_project/settings.py
+++ be/src/posapp_be_project/posapp_be_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ul_avvga$z1ur0=mv(ntmgi45x6ixjgel+^!aw0$w#mhe00=b9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.99.100"]
 
 
 # Application definition
---
+++ documentation/documentation.md
@@ -0,0 +1,12 @@
+API documentation
+
+/api/
+api root
+
+/api/profile/
+  - GET (list all profiles)
+  - POST (create new profile)
+
+/api/profile/<profile_id>
+  - GET (get profile details)
+  - POST ()
