2083688d2bea6c91ebe341ef6e227851344439ef
WINDOWS-VIJUBLD\VirtualReality <vecernikjosef@gmail.com>
2018-10-09 17:51:15 +0530

    updated dolibar BE

--- fe-android/PosApp/app/src/main/java/com/example/vaibhavchahal93788/myapplication/billdesk/utility/Constants.java
+++ fe-android/PosApp/app/src/main/java/com/example/vaibhavchahal93788/myapplication/billdesk/utility/Constants.java
@@ -2,6 +2,6 @@ package com.example.vaibhavchahal93788.myapplication.billdesk.utility;
 
 public class Constants {
 
-    public static final String SERVER_BASE_URL = "http://52.172.129.14:80/api/index.php/";
-    public static final String API_KEY = "F33oMgTwc852jA9DvdaFh2XV0SdyC3E4";
+    public static final String SERVER_BASE_URL = "http://52.172.136.166:80/api/index.php/";
+    public static final String API_KEY = "6QIh6Pm2m1L6TV7l6rY9RXxZmd69nKvs";
 }
