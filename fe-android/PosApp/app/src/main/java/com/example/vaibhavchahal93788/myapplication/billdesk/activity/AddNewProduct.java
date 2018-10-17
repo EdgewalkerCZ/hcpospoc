b936526a531831b92b98497069b95a2d7f066aab
jai.mishra1000006 <jai.mishra1000006@homecredit.co.in>
2018-10-17 18:00:20 +0530

    * Add New Product Page/ Submittion / User Profile Page

---
+++ fe-android/PosApp/app/src/main/java/com/example/vaibhavchahal93788/myapplication/billdesk/activity/AddNewProduct.java
@@ -0,0 +1,16 @@
+package com.example.vaibhavchahal93788.myapplication.billdesk.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.vaibhavchahal93788.myapplication.R;
+
+public class AddNewProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_product);
+    }
+}
