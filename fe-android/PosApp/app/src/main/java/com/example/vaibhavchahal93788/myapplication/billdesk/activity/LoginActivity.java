083fddbc2c6df2532c1dee147eefbbee0ccca8dc
amit.agrahari117137 <amit.agrahari117137@homecredit.co.in>
2018-10-22 17:39:51 +0530

    * Added loader in login.

--- fe-android/PosApp/app/src/main/java/com/example/vaibhavchahal93788/myapplication/billdesk/activity/LoginActivity.java
+++ fe-android/PosApp/app/src/main/java/com/example/vaibhavchahal93788/myapplication/billdesk/activity/LoginActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +29,7 @@ public class LoginActivity extends AppCompatActivity
     private TextView txtLogin;
     private EditText edtUsername;
     private EditText edtPassword;
+    private ProgressBar progress_bar;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,6 +39,7 @@ public class LoginActivity extends AppCompatActivity
         txtLogin = (TextView) findViewById(R.id.txtLogin);
         edtUsername = (EditText) findViewById(R.id.edtUsername);
         edtPassword = (EditText) findViewById(R.id.edtPassword);
+        progress_bar = (ProgressBar) findViewById(R.id.progress_bar);
 
 
 
@@ -54,30 +57,36 @@ public class LoginActivity extends AppCompatActivity
 
     private void validateLogin()
     {
+        progress_bar.setVisibility(View.VISIBLE);
         LoginBodyModel loginBodyModel = new LoginBodyModel();
         loginBodyModel.setUsername(edtUsername.getText().toString());
         loginBodyModel.setPassword(edtPassword.getText().toString());
 
         new LoginApiHelper().validateLogin(loginBodyModel, new IApiRequestComplete<LoginModel>() {
-
-
             @Override
             public void onSuccess(LoginModel response) {
                 if(response.getJSESSIONID() != null)
                 {
                     //for now, considering this as successful login
-                    // save this JSESSIONID for future communication
+                    //save this JSESSIONID for future communication
                     Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                     LoginActivity.this.startActivity(intent);
                     overridePendingTransition(R.anim.animation_enter,R.anim.animation_leave);
 
                     finish();
                 }
+                else
+                {
+                    onFailure("");
+                }
+                progress_bar.setVisibility(View.GONE);
             }
 
             @Override
             public void onFailure(String message) {
+                progress_bar.setVisibility(View.GONE);
                 Toast.makeText(LoginActivity.this, "Login failed : " + message, Toast.LENGTH_SHORT).show();
+
             }
         });
     }
