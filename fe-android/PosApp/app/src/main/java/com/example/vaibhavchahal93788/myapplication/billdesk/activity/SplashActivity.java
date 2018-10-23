8482bfe7d6a12e3bc0f6116cb3088c2dc1e89b1d
ashishsinghHC <Shivay1301>
2018-10-23 12:53:55 +0530

    splash changes

--- fe-android/PosApp/app/src/main/java/com/example/vaibhavchahal93788/myapplication/billdesk/activity/SplashActivity.java
+++ fe-android/PosApp/app/src/main/java/com/example/vaibhavchahal93788/myapplication/billdesk/activity/SplashActivity.java
@@ -36,7 +36,7 @@ public class SplashActivity extends AppCompatActivity
                 }
                 else
                 {
-                    Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                     SplashActivity.this.startActivity(intent);
                     overridePendingTransition(R.anim.animation_enter,R.anim.animation_leave);
                 }
