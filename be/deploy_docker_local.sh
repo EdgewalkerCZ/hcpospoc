4e220c5ec2eae02c9ecd3c2412cf99c9c68f603f
WINDOWS-VIJUBLD\VirtualReality <vecernikjosef@gmail.com>
2018-10-23 15:34:14 +0530

    Deployment changes

---
+++ be/deploy_docker_local.sh
@@ -0,0 +1,12 @@
+for i in 10; do
+    echo "###"
+done
+echo "#####################"
+echo "DOCKER IS RUNNING ON:"
+echo `docker-machinedocker ip`
+echo "#####################"
+for i in 10; do
+    echo "###"
+done
+
+winpty docker run -it -p 8080:80 hcpospoc.azurecr.io/hcpospoc:test
