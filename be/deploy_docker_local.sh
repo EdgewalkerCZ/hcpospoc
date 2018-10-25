be576ec2156de7d0a916067f14f18ee66e949e6a
WINDOWS-VIJUBLD\VirtualReality <vecernikjosef@gmail.com>
2018-10-25 16:31:33 +0530

    finalizing be build and deploy

--- be/deploy_docker_local.sh
+++ be/deploy_docker_local.sh
@@ -9,4 +9,7 @@ for i in 10; do
     echo "###"
 done
 
-winpty docker run -it -p 8080:80 hcpospoc.azurecr.io/hcpospoc:test
+#Make sure we are not running the container yet
+
+
+winpty docker run -it -p 8080:8080 -p 80:80 -p 8081:8081  hcpospoc.azurecr.io/hcpospoc:test
