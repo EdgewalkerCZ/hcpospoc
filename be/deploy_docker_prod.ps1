cc0e0107acefbc7145248890f167c10804be3afa
WINDOWS-VIJUBLD\VirtualReality <vecernikjosef@gmail.com>
2018-10-25 10:09:01 +0530

    updated deployment of tomcat apps

--- be/deploy_docker_prod.ps1
+++ be/deploy_docker_prod.ps1
@@ -1,14 +1,14 @@
 Write-Host "Starting docker-machine for good measure"
-docker-machine start
+#docker-machine start
 
 Write-Host "Login to acr"
-az acr login --name hcpospoc
+#az acr login --name hcpospoc
 
 Write-Host "Pushing the docker image to Azure"
-docker push hcpospoc.azurecr.io/hcpospoc:test
+#docker push hcpospoc.azurecr.io/hcpospoc:test
 
 Write-Host "Checking if the image is there..."
-az acr repository list --name hcpospoc --output table
+#az acr repository list --name hcpospoc --output table
 
 $acr_password = (az acr credential show --name hcpospoc --query passwords[0].value)
 
@@ -16,7 +16,7 @@ Write-Host "ACR PASSWORD is:"
 Write-Host $acr_password
 
 Write-Host "Creating Azure Container Instance"
-az container create --resource-group HCIN --name hcin-hcpospoc --image hcpospoc.azurecr.io/hcpospoc:test --cpu 1 --memory 1 --registry-login-server hcpospoc.azurecr.io --registry-username hcpospoc --registry-password $acr_password --dns-name-label hcinhcpospoc --ports 80
+az container create --resource-group HCIN --name hcin-hcpospoc --image hcpospoc.azurecr.io/hcpospoc:test --cpu 1 --memory 1 --registry-login-server hcpospoc.azurecr.io --registry-username hcpospoc --registry-password $acr_password --dns-name-label hcinhcpospoc --ports 80 8080
 
 Write-Host "Checking Container Status"
 
